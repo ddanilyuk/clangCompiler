6734bd5bbda91bb47ef6b4ea47d288ba2c7ffaa6
ddanilyuk <danis.danilyuk@gmail.com>
2020-09-22 17:24:53 +0300

    Initial Commit

---
+++ lab1SP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab1SP
+//
+//  Created by Денис Данилюк on 22.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
