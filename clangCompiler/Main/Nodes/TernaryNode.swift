7dffab4d1116c3ad18226c91528cc819061f62a2
ddanilyuk <danis.danilyuk@gmail.com>
2020-10-28 13:32:01 +0200

    add condition node

--- clangCompiler/Main/Nodes/TernaryNode.swift
+++ clangCompiler/Main/Nodes/TernaryNode.swift
@@ -15,12 +15,13 @@ struct TernaryNode: Node {
     var falseNode: Node
     
     init(conditionNode: Node, trueNode: Node, falseNode: Node) {
-        self.conditionNode =  TernaryInsideNode(node: conditionNode, positionName: "condition node")
+        self.conditionNode = TernaryInsideNode(node: conditionNode, positionName: "condition")
         self.trueNode = TernaryInsideNode(node: trueNode, positionName: "if true")
         self.falseNode = TernaryInsideNode(node: falseNode, positionName: "if false")
     }
     
     func interpret(isCPPCode: Bool) throws -> String {
+        // Implement THIS function
         return ""
     }
 }
